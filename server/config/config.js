fd06c69dd37f3ca02225be26eb5993094e9c30d4
Bryan <import.bryan.cosx@gmail.com>
2020-11-30 12:28:01 -0600

    Rest Serve - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 // =========================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+
+// =========================================
+// Entorno
+// =========================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =========================================
+// Base de atos
+// =========================================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://strider:xtU2ZuZnqRA8q0nq@cluster0.15wnd.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
